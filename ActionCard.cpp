e5b4955a64e3630bf3f943acafd99c7ed140c58a
anitaprova <104943181+anitaprova@users.noreply.github.com>
2023-09-25 11:42:39 -0400

    updating progress

--- ActionCard.cpp
+++ ActionCard.cpp
@@ -2,10 +2,17 @@
 
 ActionCard::ActionCard()
 {
-	set_type("Action");
+	set_type("action card");
+	set_text("");
+}
+
+ActionCard::ActionCard(std::string p, std::string t, std::vector<int> v)
+{
+	set_type(p);
+	set_text(t);
+	set_image(v);
 }
 
 void ActionCard::play()
 {
-
 }
--- ActionCard.h
+++ ActionCard.h
@@ -1,9 +1,10 @@
 #pragma once
 #include "Card.h"
 
-class ActionCard : Card
+class ActionCard : public Card
 {
 public:
 	ActionCard();
+	ActionCard(std::string p, std::string t, std::vector<int> v);
 	void play();
 };
